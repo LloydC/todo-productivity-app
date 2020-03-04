136cb1f50f0df7d4d8a14ff5a22abd8e5bde4ce9
Lloyd Chambrier <lloydchambrier@gmail.com>
2020-03-04 13:10:00 +0100

    App layour done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Todo Productivity App
       </header>
+      
+      <div className="container">
+        <div className="first-section">
+          <b>Next 7 days</b>
+        </div>
+
+        <div className="second-section">
+          <p><b>Next 7 days</b></p>
+          <p>Monday</p>
+          <p>Tuesday</p>
+          <p>Wednesday</p>
+          <p>Thursday</p>
+          <p>Friday</p>
+          <p>Saturday</p>
+          <p>Sunday</p>
+        </div>
+        <div className="gap">
+          gap
+        </div>
+      </div>
+      
+      <footer className="App-footer">
+
+      </footer>
     </div>
   );
 }
